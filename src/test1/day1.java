6def5519e37752e7532864d125cfb076053a3751
User <User@192.168.1.53>
2021-02-17 21:35:31 +0530

    Updated

--- src/test1/day1.java
+++ src/test1/day1.java
@@ -4,6 +4,7 @@ public class day1 {
 
 	public static void main(String[] args) {
 		System.out.println("Prashanth working in 20 projects in thought frameworks company");
+		System.out.println("hello");
 		
 
 	}
